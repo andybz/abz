19ff74b742192136e7813d1d78b8ea5b66116cdb
andybz <iamandybrazil@gmail.com>
2022-04-17 12:56:06 -0400

    add bg animation

--- templates/temp-home.php
+++ templates/temp-home.php
@@ -120,7 +120,21 @@
   </div>
 </div>
 </main>
+<div id="bg-image"></div>
+<!-- bg animation -->
+<script>
+const bg = document.querySelector('#bg-image');
+const windowWidth = window.innerWidth / 5;
+const windowHeight = window.innerHeight / 5 ;
 
+bg.addEventListener('mousemove', (e) => {
+  const mouseX = e.clientX / windowWidth;
+  const mouseY = e.clientY / windowHeight;
+  
+  bg.style.transform = `translate3d(-${mouseX}%, -${mouseY}%, 0)`;
+});
+</script>
+<!-- text animation -->
 <script>
 anime({
   targets: '#amazing path',
